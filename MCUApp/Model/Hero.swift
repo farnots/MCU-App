ca512aa2623711f8463bdaa3e0f630e405155d02
Lucas Tarasconi <lucas.tarasconi42@gmail.com>
2018-12-15 15:17:17 +1100

    Creating the model

---
+++ MCUApp/Model/Hero.swift
@@ -0,0 +1,54 @@
+//
+//  Hero.swift
+//  MCUApp
+//
+//  Created by Lucas Tarasconi on 15/12/2018.
+//  Copyright © 2018 Lucas Tarasconi. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+enum SerializationError: Error {
+    case missing(String)
+}
+
+class Hero {
+    let id: Int
+    let name: String
+    let description: String
+    let image: UIImage
+    
+    init(id: Int, name: String, description: String, image: UIImage) {
+        self.id = id
+        self.name = name
+        self.image = image
+        self.description = description
+    }
+    
+    init?(json: [String: Any]) throws {
+        guard let id = json["id"] as? Int else {
+            throw SerializationError.missing("id")
+        }
+        guard let name = json["name"] as? String else {
+            throw SerializationError.missing("name")
+        }
+        guard let description = json["description"] as? String else {
+            throw SerializationError.missing("description")
+        }
+        guard let thumbnail = json["thumbnail"] as? [String : Any],
+            let imagePath = thumbnail["path"] as? String,
+            let imageExt = thumbnail["extension"] as? String else {
+                throw SerializationError.missing("thumbnail")
+        }
+        
+        self.id = id
+        self.name = name
+        self.description = description
+        let imageURL = URL(string: imagePath + "." + imageExt)!
+        let imageData = try! Data(contentsOf: imageURL)
+        self.image = UIImage(data: imageData)!
+        
+        
+    }
+}
---
+++ MCUApp/Model/MarvelHeroes.swift
@@ -0,0 +1,31 @@
+//
+//  MarvelHeroes.swift
+//  MCUApp
+//
+//  Created by Lucas Tarasconi on 15/12/2018.
+//  Copyright © 2018 Lucas Tarasconi. All rights reserved.
+//
+
+import Foundation
+
+
+class MarvelHeroes {
+    var heroes: [Hero] = []
+    
+    init(json: [[String: Any]]) throws {
+        for hero in json {
+            do {
+                guard let currentHero = try Hero(json: hero) else {
+                    throw SerializationError.missing("Hero")
+                }
+                self.addHero(hero: currentHero)
+            } catch let error {
+                print(error)
+            }
+        }
+    }
+    
+    func addHero(hero: Hero) {
+        heroes.append(hero)
+    }
+}
